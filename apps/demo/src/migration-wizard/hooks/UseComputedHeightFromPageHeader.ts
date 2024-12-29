8f96c99c90089f13f4e1bc1457b0633229b1419a
liatb-rh <lberkovi@redhat.com>
2024-12-29 17:08:15 +0200

    Update UseComputedHeightFromPageHeader.ts

--- apps/demo/src/migration-wizard/hooks/UseComputedHeightFromPageHeader.ts
+++ apps/demo/src/migration-wizard/hooks/UseComputedHeightFromPageHeader.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useWindowSize } from "react-use";
 
-const DEFAULT_HEIGHT = 635;
+let DEFAULT_HEIGHT = 635;
 
 function getMainPageSectionVerticalPadding(pageMainSection: Element): number {
   const { paddingTop, paddingBottom } = getComputedStyle(pageMainSection);
